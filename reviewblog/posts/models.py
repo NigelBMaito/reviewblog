ab0b2cd5f1754bde2ca5e5f2ac7884dc124e0705
NigelBMaito <nigelmaito@gmail.com>
2024-12-30 20:58:47 +0000

    initial pages

--- reviewblog/posts/models.py
+++ reviewblog/posts/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date =models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(upload_to='banners/', blank = True, null=True)
 
     def __str__(self):
         return self.title
